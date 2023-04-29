a4c9e5080ad03429dd2b5db9718ca3e24917d2fb
Jack Oats <jackoatmeals@gmail.com>
2023-04-28 23:45:40 -0500

    implemented CRUD ops for user on the frontend

--- app/components/Form/Form.tsx
+++ app/components/Form/Form.tsx
@@ -1,19 +1,106 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import TextField from "./TextField/TextField";
 import styles from './form.module.css'
 
 export default function Form() {
     const [user, setUser] = useState({} as User)
+    const [newInfo, setNewInfo] = useState({} as User)
+    const [isUpdating, setIsUpdating] = useState(false)
 
-    function handleChange(newInfo: UserPartial) {
+    function handleChange(mutateUser: UserPartial) {
         setUser({
             ...user,
-            ...newInfo
+            ...mutateUser
         })
     }
 
+    // For PATCH
+    function handleNewInfo(mutateNewInfo: UserPartial) {
+        setNewInfo({
+            ...newInfo, 
+            ...mutateNewInfo
+        })
+    }
+
+    async function createUser(e: FormEvent) {
+        e.preventDefault()
+        try {
+            const res = await fetch('/api/user', {
+                method: 'POST',
+                body: JSON.stringify(user),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            
+            if (!res.ok) {
+                console.log(await res.text())
+            }
+
+            console.log(await res.json())
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function updateUser(e: FormEvent) {
+        e.preventDefault()
+        try {
+            const res = await fetch('/api/user', {
+                method: 'PATCH',
+                body: JSON.stringify({
+                    ...user,
+                    newEmail: newInfo.email,
+                    newUsername: newInfo.username,
+                    newPassword: newInfo.password,
+                }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            
+            if (!res.ok) {
+                console.log(await res.text())
+            }
+            
+            console.log(await res.json())
+            
+        } catch (error) {
+            const err = error as Error
+            console.log(err.message)
+        }
+    }
+
+    async function deleteUser(e: FormEvent) {
+        e.preventDefault()
+
+        try {
+            const res = await fetch('/api/user', {
+                method: 'DELETE',
+                body: JSON.stringify(user),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            
+            if (!res.ok) {
+                console.log(await res.text())
+            }
+            
+            console.log(await res.json())
+            
+        } catch (error) {
+            const err = error as Error
+            console.log(err.message)
+        }
+    }
+
+
     return (
-        <form className={styles['container']}>
+        <form className={styles['container']} onSubmit={
+            isUpdating ? updateUser : deleteUser
+        }>
             <TextField
                 onChange={handleChange}
                 value={user}
@@ -35,6 +122,37 @@ export default function Form() {
                 id={'password'}
                 type={'password'}
             />
+            {isUpdating && (
+                <>
+                    <TextField
+                        onChange={handleNewInfo}
+                        value={newInfo}
+                        label={'New Username?'}
+                        id={'username'}
+                        type={'text'}
+                    />
+                    <TextField
+                        onChange={handleNewInfo}
+                        value={newInfo}
+                        label={'New Email?'}
+                        id={'email'}
+                        type={'text'}
+                    />
+                    <TextField
+                        onChange={handleNewInfo}
+                        value={newInfo}
+                        label={'New Password?'}
+                        id={'password'}
+                        type={'password'}
+                    />
+                </>
+            )}
+            <div onClick={() => setIsUpdating(!isUpdating)}>
+                Click to change
+            </div>
+            <button>
+                {isUpdating ? 'Update' : 'Create'}
+            </button>
         </form>
     )
 }
---
+++ lib/isEmail.ts
@@ -0,0 +1,4 @@
+export default function isEmail(email: string): email is Email {
+    const regex = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+    return regex.test(email)
+}
